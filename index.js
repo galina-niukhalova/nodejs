275b2330b84e66e6f49eee20f88e700c68e21c23
Galina <galina.hvan1011@gmail.com>
2020-01-28 13:52:06 +0800

    html templates

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+function replaceTemplate(temp, {
+  image, productName, quantity, price, from, nutrients, description, organic, id,
+}) {
+  const output = temp
+    .replace(/{%IMAGE%}/g, image)
+    .replace(/{%PRODUCT_NAME%}/g, productName)
+    .replace(/{%QUANTITY%}/g, quantity)
+    .replace(/{%PRICE%}/g, price)
+    .replace(/{%FROM%}/g, from)
+    .replace(/{%NUTRIENCE%}/g, nutrients)
+    .replace(/{%DESCRIPTION%}/g, description)
+    .replace(/{%ID%}/g, id);
+
+  if (!organic) {
+    output.replace(/NOT_ORGANIC/, 'not-organic');
+  }
+
+  return output;
+}
+
+const cardsHTML = dataObj.map((product) => replaceTemplate(tempCard, product));
+const overviewHTML = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
 
 const server = http.createServer((req, res) => {
-  const pathname = req.url;
+  // true = parseQueryString, query will be object instead of string
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
 
-    res.end('Overview page');
+    res.end(overviewHTML);
 
   // Product page
   } else if (pathname === '/product') {
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
-    res.end('Product page');
+    const productHTML = replaceTemplate(tempProduct, dataObj[query.id]);
+    res.end(productHTML);
 
   // API
   } else if (pathname === '/api') {
