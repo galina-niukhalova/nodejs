34362f84598ed2a95b234377f15c1403183c72f4
Galina <galina.hvan1011@gmail.com>
2020-01-30 20:10:31 +0800

    prettier

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ const url = require('url');
 // const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 // const slugs = dataObj.map(({ productName }) => slugify(productName, { lower: true }));
 
-const cardsHTML = dataObj.map((product) => replaceTemplate(tempCard, product));
+const cardsHTML = dataObj.map(product => replaceTemplate(tempCard, product));
 const overviewHTML = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
 
 const server = http.createServer((req, res) => {
@@ -22,23 +31,23 @@ const server = http.createServer((req, res) => {
   // Overview page
   if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {
-      'Content-type': 'text/html',
+      'Content-type': 'text/html'
     });
 
     res.end(overviewHTML);
 
-  // Product page
+    // Product page
   } else if (pathname === '/product') {
     res.writeHead(200, {
-      'Content-type': 'text/html',
+      'Content-type': 'text/html'
     });
     const productHTML = replaceTemplate(tempProduct, dataObj[query.id]);
     res.end(productHTML);
 
-  // API
+    // API
   } else if (pathname === '/api') {
     res.writeHead(200, {
-      'Content-type': 'application/json',
+      'Content-type': 'application/json'
     });
     res.end(data);
 
@@ -46,7 +55,7 @@ const server = http.createServer((req, res) => {
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
-      'my-own-header': 'hello-world',
+      'my-own-header': 'hello-world'
     });
     res.end('<h1>Page not found!</h1>');
   }
