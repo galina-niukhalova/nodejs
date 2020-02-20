163ecb86b9e6de6fb0b1a993ab86f9e06c01fdb5
Galina <galina.hvan1011@gmail.com>
2020-02-20 17:07:41 +0800

    mongoose

--- natours/server.js
+++ natours/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
@@ -5,6 +6,47 @@ dotenv.config({
 });
 
 const app = require('./app');
+
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  }).then(() => {
+    console.log('DB connection successful!');
+  });
+
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // validator (the second param is an error message)
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
+const testTour = new Tour({
+  name: 'The Sea Explorer',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour.save()
+  .then((doc) => console.log(doc))
+  .catch((er) => console.log(er));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
